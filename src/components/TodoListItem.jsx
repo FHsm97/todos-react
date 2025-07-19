05f38c98090210faf2e4d9dee5a960d75423a2d6
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-07-19 11:31:10 +0330

    edit mode part-1 done!

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import DeleteIcon from "./icons/DeleteIcon";
 import EditIcon from "./icons/EditIcon";
 
-export default function TodoListItem({todo,deleteTodo,toggleTodo}) {
+export default function TodoListItem({ todo, deleteTodo, toggleTodo }) {
+    const [editMode, setEditMode] = useState(false);
+
+
+
+
     return (
         <li className="relative flex items-center justify-between px-2 py-6 border-b">
-            <div>
-                <input type="checkbox" checked={todo?.status} onChange={()=>toggleTodo(todo)} className="" />
-                <p className={`inline-block mt-1 ml-2 text-gray-600 ${todo?.status?'line-through':''}`}>{todo?.title}</p>
-            </div>
-            <button type="button" className="absolute right-0 flex items-center space-x-1">
-                <EditIcon/>
-                <DeleteIcon onClick={()=>deleteTodo(todo)}/>
-            </button>
+            {/*  */}
+
+
+
+
+
+            {
+                editMode
+                    ?
+                    <div className="w-full flex items-center">
+                        <input type="text" value={todo?.title} onChange={() => { }} className="w-full px-4 py-2 border border-gray-200 rounded" />
+                        <DeleteIcon className="ml-2" onClick={() => setEditMode(false)} />
+                    </div>
+                    :
+                    (
+                        <div className="flex items-center">
+                            <div>
+                                <input type="checkbox" checked={todo?.status} onChange={() => toggleTodo(todo)} className="" />
+                                <p className={`inline-block mt-1 ml-2 text-gray-600 ${todo?.status ? 'line-through' : ''}`}>{todo?.title}</p>
+                            </div>
+                            <button type="button" className="absolute right-0 flex items-center space-x-1">
+                                <EditIcon onClick={()=>setEditMode(true)} />
+                                <DeleteIcon onClick={() => deleteTodo(todo)} />
+                            </button>
+                        </div>
+                    )
+            }
         </li>
 
     )
